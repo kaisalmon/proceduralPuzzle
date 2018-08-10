f4be910ee73cf74ead7cb681ca29620d8393e3fa
117229 <u86036338@cbi-bs1.server.lan>
2018-08-10 20:43:44 +0200

    sw changes

--- sw.js
+++ sw.js
@@ -21,6 +21,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
+	console.log(event)
       return caches.match(event.request,{ignoreSearch:true});
     })
   );
