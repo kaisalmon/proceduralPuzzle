10745d732dfd5a1fd9425f9141b8bd638c384b05
Kai Salmon <kaisalmon@hotmail.com>
2018-08-10 17:03:30 +0200

    Allowed service workers to work with query strings

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return caches.match(event.request);
+      return caches.match(event.request,{ignoreSearch:true});
     })
   );
 });
