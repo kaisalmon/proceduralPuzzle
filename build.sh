303489a67c05254138c77a6f4f7a6e9aa620472d
Kai Salmon <kaisalmon@hotmail.com>
2018-08-10 16:37:17 +0200

    Maybe added service worker?

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 tsc -p tsconfig.json &&
 browserify main/main.js -o js/main.js &&
 browserify  -t vueify  main/settings.js -o js/settings.js &&
-rm main/*.js && rm main/*.*.map
+rm main/*.js && rm main/*.*.map && rm js/*.js.tmp*
---
+++ js/sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open("cache").then(function(cache) {
+      return cache.addAll(
+        [
+          'animate.css',
+          'material-switch.css',
+          'bootstrap.min.css',
+          'https://fonts.googleapis.com/css?family=Josefin+Sans',
+          'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+          'js/settings.js',
+          'js/main.js'
+        ]
+      );
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
