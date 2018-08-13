061d6897e35df95070615df0c19266f12bb2e132
Kai Salmon <kaisalmon@hotmail.com>
2018-08-13 12:55:48 +0200

    No external files

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-console.log("A");
-self.addEventListener('install', function(event) {
+/*
+global caches, self, addEventListener, CACHE_DYNAMIC_NAME, CACHE_CONTAINING_ERROR_MESSAGES, fetch
+*/
+
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open("cache").then(function(cache) {
+    caches.open('cache').then(function (cache) {
       let arr = [
         './animate.css',
         './material-switch.css',
@@ -14,45 +17,46 @@ self.addEventListener('install', function(event) {
         './',
         'assets/cracked.png',
         'assets/cracked64.png',
-        './game.html'
-      ];
-      for(var i in arr){
+        './game.html',
+        'fonts/JosefinSans-Regular.ttf',
+        'fonts/JosefinSans-Bold.ttf',
+        'fonts/all.min.css',
+        'fonts/fontawesome.css',
+        'webfonts/fa-solid-900.ttf'
+      ]
+      for (var i in arr) {
         let e = arr[i]
-        console.log("caching", e);
         cache.add(e)
       }
-      return cache;
+      return cache
     })
-  );
-});
-console.log("C");
+  )
+})
 
-addEventListener('fetch', function(event) {
-  console.log("D");
+addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request, {ignoreSearch: true})
-      .then(function(response) {
-        console.log("E");
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              console.log("F");
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('./index.html');
-                });
-            });
-        }
-      })
-  );
-});
-console.log("G");
+    caches.match(event.request, {
+      ignoreSearch: true
+    }).then(function (response) {
+      console.log('E')
+      if (response) {
+        return response // if valid response is found in cache return it
+      } else {
+        return fetch(event.request) // fetch from internet
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME)
+              .then(function (cache) {
+                cache.put(event.request.url, res.clone()) // save the response for future
+                return res // return the fetched data
+              })
+          })
+          .catch(function () { // fallback mechanism
+            return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+              .then(function (cache) {
+                return cache.match('./index.html')
+              })
+          })
+      }
+    })
+  )
+})
