824170acdb3f4e9e543944f9c2dbff32a4766430
aaron <aaron@coinmode.com>
2019-03-11 17:39:20 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ browserify --im  -t vueify  main/settings.js -o js/settings.js &&
 echo "done browerfiying" &&
 cp main/*.js cmd &&
 perl -pi -e 's/main/cmd/g' cmd/*.js &&
+return 0
 #uglifyjs js/main.js --compress -o js/main.min.js &&
 #uglifyjs js/settings.js --compress -o js/settings.min.js &&
 #echo "done uglifying"
